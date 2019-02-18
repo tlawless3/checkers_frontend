334becaaeaa676e9a1c53b406924311d0672de42
Thomas Lawless <tlawless94@gmail.com>
2019-02-18 16:42:41 -0600

    user role determines view possiblities

--- src/App.js
+++ src/App.js
@@ -6,26 +6,36 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      userRole: 'loading'
+    }
+    this.verifyUser = this.verifyUser.bind(this)
+  }
 
   async verifyUser() {
     try {
-      let dataJSON = {
-        "user": {
-          "username": "newnew",
-          "password": "password"
-        }
-      }
-      // console.log(await axios.get(process.env.REACT_APP_SERVER_URL + '/api/v1.0.0/testApi'))
-      await axios.post(process.env.REACT_APP_SERVER_URL + '/api/v1.0.0/user/login', dataJSON, {
+      // let dataJSON = {
+      //   "user": {
+      //     "username": "newnew",
+      //     "password": "password"
+      //   }
+      // }
+      // // console.log(await axios.get(process.env.REACT_APP_SERVER_URL + '/api/v1.0.0/testApi'))
+      // await axios.post(process.env.REACT_APP_SERVER_URL + '/api/v1.0.0/user/login', dataJSON, {
+      //   withCredentials: true
+      // })
+      let result = await axios.get(process.env.REACT_APP_SERVER_URL + '/api/v1.0.0/user/verify', {
         withCredentials: true
       })
-      await axios.get(process.env.REACT_APP_SERVER_URL + '/api/v1.0.0/user/verify', {
-        withCredentials: true
-      })
+      this.setState({ userRole: result.data.role })
     } catch (err) {
+      this.setState({ userRole: 'none' })
       console.log(err)
     }
   }
+
   componentDidMount() {
     this.verifyUser()
   }
@@ -33,7 +43,7 @@ class App extends Component {
   render() {
 
     return (
-      true ?
+      this.state.userRole === 'user' ?
         (<div className="App" >
           loggedIn
          </div>
