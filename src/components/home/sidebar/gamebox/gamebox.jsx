f50009e768b56ee628157e9fdc62743d43b6b8eb
Thomas Lawless <tlawless94@gmail.com>
2019-03-01 09:39:47 -0600

    sidebar styling

--- src/components/home/sidebar/gamebox/gamebox.jsx
+++ src/components/home/sidebar/gamebox/gamebox.jsx
@@ -5,16 +5,15 @@ import { Board } from '../../../index'
 const Gamebox = (props) => {
   return (
     <div className='boxWrapper' onClick={() => props.setActiveGame(props.game.id)}>
-      <Board board={props.game.board} active={false} resolution={80} />
+      <div className='boardWrapper'>
+        <Board board={props.game.board} active={false} resolution={80} />
+      </div>
       <div className='gameInfo'>
         <div>
           {props.game.playerColors.black === props.user.userId ? 'Black' : 'Red'}
         </div>
         <div>
           Status: {props.game.status}
-        </div>
-        <div>
-          {props.game.id}
         </div>
       </div>
     </div>
