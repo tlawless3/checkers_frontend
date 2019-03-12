f59b4fe5380b4bee9a3a8c1c3a8e620e96fe3e2d
Thomas Lawless <tlawless94@gmail.com>
2019-03-12 18:10:23 -0500

    scaffold form for creating game

--- src/components/createGame/createGame.jsx
+++ src/components/createGame/createGame.jsx
@@ -3,6 +3,7 @@ import React, {
 } from 'react';
 import Navbar from '../navbar/navbar'
 import { connect } from 'react-redux'
+import { fetchFriends } from '../../actions/friend'
 import './createGame.css';
 
 class CreateGame extends Component {
@@ -13,11 +14,47 @@ class CreateGame extends Component {
     }
   }
 
+  handleSubmit() {
+
+  }
+
+  handleChange(event) {
+    const key = event.target.name
+    const value = event.target.value
+    const stateObj = {}
+    stateObj[key] = value
+    this.setState(stateObj)
+  }
+
   render() {
     return (
       <div className='createGamePageWrapper'>
         <Navbar />
-        createGame
+        <div className='createGameFormWrapper'>
+          <form id='createGameForm'>
+            <select name="boardSize" onChange={this.handleChange} form="createGameForm">
+              <option value="8">8</option>
+              <option value="10">10</option>
+            </select>
+            <select name="color" onChange={this.handleChange} form="createGameForm">
+              <option value="red">red</option>
+              <option value="black">black</option>
+            </select>
+            <select name="opponent" onChange={this.handleChange} form="createGameForm">
+              <option value="ai">AI</option>
+              <option value="friend">friend</option>
+            </select>
+            {this.state.opponent === 'friend' ? (
+              <div>
+                <div>
+                  Enter a friend's username:
+                </div>
+                <input onChange={this.handleChange} type='text'></input>
+              </div>
+            ) : ''}
+            <button type='submit'> Create Game </button>
+          </form>
+        </div>
       </div>
     )
   }
@@ -28,6 +65,7 @@ const mapStateToProps = state => ({
 })
 
 const mapDispatchToProps = dispatch => ({
+  fetchFriends: () => dispatch(fetchFriends()),
 })
 
 export default connect(mapStateToProps, mapDispatchToProps)(CreateGame);
